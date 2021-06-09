b46016d6af2c9b3d3b096211f2835c61b3f0bb2c
Aocheng Wang <aochengwang@microsoft.com>
2021-06-09 16:59:05 +0800

    add test code for exit code

--- task-provider-sample/src/extension.ts
+++ task-provider-sample/src/extension.ts
@@ -1,29 +1,28 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 import * as vscode from 'vscode';
-import { RakeTaskProvider } from './rakeTaskProvider';
-import { CustomBuildTaskProvider } from './customTaskProvider';
 
-let rakeTaskProvider: vscode.Disposable | undefined;
-let customTaskProvider: vscode.Disposable | undefined;
-
-export function activate(_context: vscode.ExtensionContext): void {
-	const workspaceRoot = vscode.workspace.rootPath;
-	if (!workspaceRoot) {
-		return;
+export class SimpleTaskProvider implements vscode.TaskProvider {
+	static TaskType = 'simple task';
+	public provideTasks(): Thenable<vscode.Task[]> | undefined {
+		return Promise.resolve([
+			new vscode.Task(
+				{type: SimpleTaskProvider.TaskType},
+				 vscode.TaskScope.Workspace,
+				"sleep",
+				SimpleTaskProvider.TaskType,
+				new vscode.ShellExecution("echo 'start to sleep' && sleep 10000000"))
+		]);
 	}
-		
-	rakeTaskProvider = vscode.tasks.registerTaskProvider(RakeTaskProvider.RakeType, new RakeTaskProvider(workspaceRoot));
-	customTaskProvider = vscode.tasks.registerTaskProvider(CustomBuildTaskProvider.CustomBuildScriptType, new CustomBuildTaskProvider(workspaceRoot));
+
+	public resolveTask(task: vscode.Task): vscode.Task | undefined {
+		return undefined;
+	}
 }
 
-export function deactivate(): void {
-	if (rakeTaskProvider) {
-		rakeTaskProvider.dispose();
-	}
-	if (customTaskProvider) {
-		customTaskProvider.dispose();
-	}
+export function activate(_context: vscode.ExtensionContext): void {
+	vscode.tasks.registerTaskProvider(SimpleTaskProvider.TaskType, new SimpleTaskProvider());
+	_context.subscriptions.push(vscode.tasks.onDidEndTaskProcess(onDidEndTaskProcessHandler));
+}
+
+function onDidEndTaskProcessHandler(event: vscode.TaskProcessEndEvent): void {
+	console.log(`task '${event.execution.task.name}' ends, exit code = '${event.exitCode}'`);
 }
